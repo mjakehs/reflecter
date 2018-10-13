196e43fb1457f22274a19c6c36c2c61236e2c09b
Jake Marzitelli <mjakehs@gmail.com>
2018-10-12 20:45:29 -0500

    refactor form.reducer.js to fit new state structure

--- src/redux/reducers/form.reducer.js
+++ src/redux/reducers/form.reducer.js
@@ -1,55 +1,20 @@
-import { combineReducers } from 'redux';
 import FORM_ACTIONS from '../actions/FORM_ACTIONS';
 
-const feeling = (state = '', action) => {
+const feedbackForm  = (state = {feeling: '', comprehension: '', supported:'', comments: ''}, action) => {
     switch(action.type){
         case FORM_ACTIONS.ADD_FEEBACK_ONE:
-            return action.payload;
-        case FORM_ACTIONS.CLEAR_FEEDBACK_FORM:
-            return '';
-        default:
-            return state;
-    }
-}
-
-const comprehension = (state = '', action) => {
-    switch(action.type){
+            return {...state, feeling: action.payload};
         case FORM_ACTIONS.ADD_FEEBACK_TWO:
-            return action.payload;
-        case FORM_ACTIONS.CLEAR_FEEDBACK_FORM:
-            return '';
-        default:
-            return state;
-    }
-}
-
-const support = (state = '', action) => {
-    switch(action.type){
+            return {...state, comprehension: action.payload};
         case FORM_ACTIONS.ADD_FEEBACK_THREE:
-            return action.payload;
-        case FORM_ACTIONS.CLEAR_FEEDBACK_FORM:
-            return '';
-        default:
-            return state;
-    }
-}
-
-const comments = (state = '', action) => {
-    switch(action.type){
+            return {...state, supported: action.payload};
         case FORM_ACTIONS.ADD_FEEBACK_FOUR:
-            return action.payload;
+            return {...state, comments: action.payload};
         case FORM_ACTIONS.CLEAR_FEEDBACK_FORM:
-            return '';
+            return {feeling: '', comprehension: '', supported:'', comments: ''};
         default:
             return state;
     }
 }
-
-const feedbackForm = combineReducers({
-    feeling,
-    comprehension,
-    support,
-    comments,
-})
 
 export default feedbackForm;
