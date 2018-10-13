dfb841f158a2f8e04da8e762171a6efde3c6fe36
Jake Marzitelli <mjakehs@gmail.com>
2018-10-13 13:57:24 -0500

    add @M-ui Icon and Button styling for next button component

--- src/components/Shared/NextButton.js
+++ src/components/Shared/NextButton.js
@@ -1,21 +1,33 @@
 //button that adds form value to state
 //takes path of next view and redux action as props
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import STAGE_ACTIONS from '../../redux/actions/STAGE_ACTIONS';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import ArrowForward from '@material-ui/icons/ArrowForward';
 
 let nextPath = '';
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    },
+    rightIcon: {
+        marginLeft: theme.spacing.unit,
+    },
+});
 
 class NextButton extends Component {
 
     handleNextAction = () => {
         this.props.history.push(nextPath)
-        this.props.dispatch({type: STAGE_ACTIONS.NEXT_STAGE})
+        this.props.dispatch({ type: STAGE_ACTIONS.NEXT_STAGE })
     }
-    
+
     setPath = props => {
-        switch(props) {
+        switch (props) {
             case 1:
                 nextPath = '/feedback/2';
                 break;
@@ -31,19 +43,22 @@ class NextButton extends Component {
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.setPath(this.props.stage);
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                <button onClick={this.handleNextAction}>Next</button>
+                <Button variant="contained" color="primary" size="large" onClick={this.handleNextAction}>
+                    Next
+                    <ArrowForward className={this.props.classes.rightIcon} />
+                </Button>
             </div>
         );
     }
 }
 
-const mapStateToProps = ({stage}) => ({stage});
+const mapStateToProps = ({ stage }) => ({ stage });
 
-export default withRouter(connect(mapStateToProps)(NextButton));
+export default withStyles(styles)(withRouter(connect(mapStateToProps)(NextButton)));
