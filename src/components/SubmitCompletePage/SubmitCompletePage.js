bc05441668bee7fb183245754c184ef01d6704de
Jake Marzitelli <mjakehs@gmail.com>
2018-10-13 13:45:18 -0500

    add @M-ui button styling for restart button component

--- src/components/SubmitCompletePage/SubmitCompletePage.js
+++ src/components/SubmitCompletePage/SubmitCompletePage.js
@@ -1,6 +1,16 @@
 //page view displayed on form completeion -- pushes user back to form start.
 import React, {Component} from 'react';
 import { withRouter } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+    button: {
+      margin: theme.spacing.unit,
+    },
+  });
+
 
 class SubmitCompletePage extends Component {
 
@@ -11,10 +21,10 @@ class SubmitCompletePage extends Component {
     render(){
         return(
             <div>
-                <button onClick={this.restartFeedbackProcess}>Return to Give Feedback Form</button>
+                <Button variant="contained" color="primary"  size="large" className={this.props.classes.button} onClick={this.restartFeedbackProcess}>Return to Give Feedback Form</Button>
             </div>
         );
     }
 }
 
-export default withRouter(SubmitCompletePage);
+export default withStyles(styles)(withRouter(SubmitCompletePage));
