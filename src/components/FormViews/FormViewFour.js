5b6d33bffe4141b13474308010ced1acd3041d0e
Jake Marzitelli <mjakehs@gmail.com>
2018-10-12 21:35:38 -0500

    made 4th form view unique, add axios post to pass feedback to api

--- src/components/FormViews/FormViewFour.js
+++ src/components/FormViews/FormViewFour.js
@@ -1,20 +1,50 @@
 import React, {Component} from 'react';
-import FeedBackInput from '../Shared/FeedbackInput';
-import NextButton from '../Shared/NextButton';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
-const prompt = 'Any Comments you want to leave?';
+//action includes
+import FORM_ACTIONS from '../../redux/actions/FORM_ACTIONS';
+import STAGE_ACTIONS from '../../redux/actions/STAGE_ACTIONS';
+
 
 class FormViewFour extends Component {
+
+    handleCommentChange = event => {
+        this.props.dispatch({type: FORM_ACTIONS.ADD_FEEBACK_FOUR, payload: event.target.value});
+    }
+
+    submitFeedback = () => {
+        axios({
+            method: 'POST',
+            url: '/api/feedback',
+            data: this.props.feedbackForm
+        })
+        .then(() => {
+            //obviosuly this code is clunky -- hopefully learning redux sagas will help :)
+            this.props.dispatch({type: STAGE_ACTIONS.RESTART_STAGE});
+            this.props.dispatch({type: FORM_ACTIONS.CLEAR_FEEDBACK_FORM});
+            this.props.history.push('/complete');
+        })
+        .catch(err => {
+            console.log(err);
+            alert('Error posting feedback');
+        })
+    }
+
 
     render(){
         return(
             <div>
                 <h1>FormViewFour</h1>
-                {/* <FeedBackInput formNumber={'four'} prompt={prompt} />
-                <NextButton nextPath={'four'} /> */}
+                <p>Any comments you want to leave?</p>
+                <input type="text" onChange={this.handleCommentChange} />
+                <button onClick={this.submitFeedback}>Submit</button>
             </div>
         );
     }
 }
 
-export default FormViewFour;
+const mapStateToProps = ({feedbackForm}) => ({feedbackForm});
+
+export default withRouter(connect(mapStateToProps)(FormViewFour));
